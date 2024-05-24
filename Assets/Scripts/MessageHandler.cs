09827ac1ed5ce1c99107521f0bd8850f8e2bab49
taopengcheng <2902630296@qq.com>
2024-05-24 20:44:34 +0800

    添加消息中心的所需

---
+++ Assets/Scripts/MessageHandler.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Threading.Tasks;
+
+
+public interface IMessageHander
+{
+    Type GetHandlerType();
+
+}
+
+[MessageHandler]
+public abstract class MessageHandler<T> : IMessageHander where T : struct
+{
+    public Type GetHandlerType()
+    {
+        return typeof(T);
+    }
+
+    public abstract Task HandleMessage(T arg);
+}
+
+[AttributeUsage(AttributeTargets.Class, Inherited = true, AllowMultiple = false)]
+sealed class MessageHandlerAttribute : Attribute { }
+
